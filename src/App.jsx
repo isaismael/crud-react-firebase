4d22d0f2c0d0b6e4582ae4a48d4bc301c9069866
Ismael Isa <ismael.isa02@gmail.com>
2023-03-23 13:19:05 -0300

    se creo funciones edi, delete y array de toda la info

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
-import './App.css'
-import { collection, query, onSnapshot, doc, updateDoc, deleteDoc } from 'firebase/firestore';
-import { db } from './data/firebase'
+import "./App.css";
+import {
+  collection,
+  query,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "./data/firebase";
+import { useState, useEffect } from "react";
 
 function App() {
+  //
+  const [todos, setTodos] = useState([]);
 
-  return (
-    <div className="App">
-      
-    </div>
-  )
+  //
+  useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let todosArray = [];
+      querySnapshot.forEach((doc) => {
+        todos.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArray);
+    });
+    return () => unsub();
+  }, []);
+
+  //
+  const handleEdit = async (todo, title) => {
+    await updateDoc(doc(db, "todos", todo.id), { title: title });
+  };
+
+  //
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "todo", id));
+  };
+
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
