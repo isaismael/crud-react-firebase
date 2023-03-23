b303edcb7cca086ddc3d0743d7283e6e19168a77
Ismael Isa <ismael.isa02@gmail.com>
2023-03-23 12:57:43 -0300

    inicio proyecto crud con firebase

---
+++ src/data/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPkkgCnRAbqeTdQ9DCrhVbR88TPq16O8I",
+  authDomain: "fir-crud-4aed3.firebaseapp.com",
+  projectId: "fir-crud-4aed3",
+  storageBucket: "fir-crud-4aed3.appspot.com",
+  messagingSenderId: "777615491767",
+  appId: "1:777615491767:web:4c263164a6a2645acb4332",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
